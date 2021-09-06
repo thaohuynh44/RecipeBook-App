07423d83deff04396fec8dd23e6f4097be4e5a8d
Thao Huynh <huynhngocthao989@gmail.com>
2021-09-06 10:24:27 +0700

    ImprovingWithSubject

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,7 +6,6 @@ import { Recipe } from "./recipe.model";
 @Injectable()
 
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe(
