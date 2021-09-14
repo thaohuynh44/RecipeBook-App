09c08a167cd451bff700790183b00d0fe3e96638
Thao Huynh <huynhngocthao989@gmail.com>
2021-09-14 16:23:43 +0700

    AddHttpService

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,27 +9,34 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Shish Kebab',
-            'This is simply a test',
-            'https://cdn.pixabay.com/photo/2014/08/14/14/21/shish-kebab-417994_1280.jpg',
-            [
-                new Ingredient('Chicken', 1),
-                new Ingredient('Yogurt', 3)
-            ]),
-        new Recipe(
-            'Grilled Marinated Meat',
-            'This is simply a test 2',
-            'https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132_1280.jpg',
-            [
-                new Ingredient('Beef steak', 1),
-                new Ingredient('Lemon', 2),
-                new Ingredient('Pepper', 3)
-            ])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Shish Kebab',
+    //         'This is simply a test',
+    //         'https://cdn.pixabay.com/photo/2014/08/14/14/21/shish-kebab-417994_1280.jpg',
+    //         [
+    //             new Ingredient('Chicken', 1),
+    //             new Ingredient('Yogurt', 3)
+    //         ]),
+    //     new Recipe(
+    //         'Grilled Marinated Meat',
+    //         'This is simply a test 2',
+    //         'https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132_1280.jpg',
+    //         [
+    //             new Ingredient('Beef steak', 1),
+    //             new Ingredient('Lemon', 2),
+    //             new Ingredient('Pepper', 3)
+    //         ])
+    // ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) {}
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes() {
         return this.recipes.slice();
