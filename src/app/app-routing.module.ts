a005212143cc25df0baea82f409166978c49c416
Thao Huynh <huynhngocthao989@gmail.com>
2021-09-17 09:32:21 +0700

    Authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthComponent } from "./auth/auth.component";
+import { AuthGuard } from "./auth/auth.guard";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipesResolverService } from "./recipes/recipe-resolver.service";
@@ -10,7 +12,11 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch:'full'},
-    { path: 'recipes', component: RecipesComponent, children: [
+    { 
+        path: 'recipes', 
+        component: RecipesComponent, 
+        canActivate: [AuthGuard],
+        children: [
         { path: '', component: RecipeStartComponent },
         { path: 'new', component: RecipeEditComponent},
         { 
@@ -24,7 +30,8 @@ const appRoutes: Routes = [
             resolve: [RecipesResolverService]
         }
     ] },
-    { path: 'shopping-list', component: ShoppingListComponent}
+    { path: 'shopping-list', component: ShoppingListComponent},
+    { path: 'auth', component: AuthComponent}
 ];
 
 @NgModule({
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map, tap } from 'rxjs/operators';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { AuthService } from "../auth/auth.service";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+    constructor(private http: HttpClient,
+        private recipeService: RecipeService,
+        private authService: AuthService) { }
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
         this.http
             .put('https://ng-course-recipe-book-dd621-default-rtdb.firebaseio.com/recipes.json',
-                  recipes
+                recipes
             )
             .subscribe(response => {
                 console.log(response);
@@ -22,15 +25,13 @@ export class DataStorageService {
     fetchRecipes() {
         return this.http
             .get<Recipe[]>('https://ng-course-recipe-book-dd621-default-rtdb.firebaseio.com/recipes.json')
-            .pipe(map(recipes => {
+             .pipe(map(recipes => {
                 return recipes.map(recipe => {
-                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-                })
+                    return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+                });
             }),
             tap(recipes => {
                 this.recipeService.setRecipes(recipes);
-            }
-
-            ))
+            }));
     }
 }
